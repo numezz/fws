1fd607b409f77681032986e3995f32f27d786b84
numezz <numezz912@gmail.com>
2024-12-10 18:55:14 +0300

    com

--- fawa-store/backend/controllers/UserControllers.js
+++ fawa-store/backend/controllers/UserControllers.js
@@ -7,8 +7,10 @@ class UserController {
             const { email, login, password } = req.body
             if (!(email.length >= 6 && login.length >= 3 && password.length >= 6)) return res.status(422).json({ message: 'Поля заполненны не верно!' })
             const userData = await UserService.registration(email, login, password)
+            res.status(200)
         } catch (error) {
             console.error(error)
+            res.status(400)
         }
     }
 
--- fawa-store/backend/services/userService.js
+++ fawa-store/backend/services/userService.js
@@ -1,10 +1,20 @@
 const UserDTO = require('../DTOs/user-DTO');
 const crypto = require('crypto')
+const UserModule = require('../models/userModule')
 
 class UserService {
     async registration(email, login, password) {
-        const hashPassword = crypto.createHash('sha256').update(password).digest('hex')
-        const userDTO = new UserDTO({ email, login, hashPassword })
+        try {
+            const hashPassword = crypto.createHash('sha256').update(password).digest('hex')
+            const userDTO = new UserDTO({ email, login, hashPassword })
+            const mongoresp = UserModule.create(userDTO)
+            console.log(mongoresp)
+            return mongoresp
+        } catch (error) {
+            return error
+
+        }
+
     }
 }
 
